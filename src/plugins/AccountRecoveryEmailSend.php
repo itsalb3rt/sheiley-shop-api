a6425d8a549649b910cc077cf02a48f9766add9d
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-19 22:24:34 -0400

    Interfaz de envio de correos de recuperacion de cuenta

---
+++ src/plugins/AccountRecoveryEmailSend.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\plugins;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class AccountRecoveryEmailSend
+{
+    private $emailDestinatary;
+    private $token;
+
+    public function __construct($emailDestinatary,$token)
+    {
+        $this->emailDestinatary = $emailDestinatary;
+        $this->token = $token;
+    }
+
+    public function send(){
+        $mail = new PHPMailer();
+        $mail->IsSMTP(); // enable SMTP
+
+        $mail->SMTPDebug = 0; //debug off = 0, debugging: 1 = errors and messages, 2 = messages only
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+        $mail->Host = "mail host";
+        $mail->Port = 465; // or 587
+        $mail->IsHTML(true);
+        $mail->Username = "serveremail@domain.com";
+        $mail->Password = "password";
+        $mail->SetFrom("serveremail@domain.com");
+        $mail->Subject = "Sheiley Shop account recovery";
+        $mail->Body = $this->getBodyMessage();
+        $mail->AddAddress($this->emailDestinatary);
+        return $mail->Send();
+    }
+
+    private function getBodyMessage(){
+        $bodyMessage = '<div>The following link will take you to an external page to recover your password;</div>';
+        $url = "https://gibucket.a2hosted.com/sheiley_shop/auth/recoverypassword/?token=$this->token";
+        $bodyMessage .= '<div><a href=" '. $url .' "> ' . $url . '</a></div>';
+        $bodyMessage .= '<div>This email expires in 2 hours.</div>';
+        return $bodyMessage;
+    }
+}
