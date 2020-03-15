cb36eaf3f29c0b2f806a6852708e6b33d1947ce2
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-03-14 21:54:14 -0400

    created taxes module

--- src/controllers/TaxesController.php
+++ src/controllers/TaxesController.php
@@ -1,10 +1,45 @@
 <?php
 
 
-namespace App\controllers;
+use App\models\Taxes\Taxes;
+use App\models\Users\Users;
+use App\plugins\RestResponse;
+use App\plugins\SecureApi;
+use Symfony\Component\HttpFoundation\Request;
 
-
-class TaxesController
+class TaxesController extends Controller
 {
+    private $request;
+    private $userToken;
+
+    public function __construct()
+    {
+        new SecureApi();
+        $this->request = Request::createFromGlobals();
+        $this->userToken = str_replace('Bearer ', '', $this->request->headers->get('Authorization'));
+    }
+
+    public function taxes()
+    {
+        $user = new Users();
+        $user = $user->getByToken($this->userToken);
+        switch ($this->request->server->get('REQUEST_METHOD')) {
+            case 'GET':
+                $taxes = new Taxes();
+                new RestResponse($taxes->get($user->id_user));
+                return;
+                break;
+            case 'PATCH':
+                $updateTax = json_decode($this->request->getContent(), true);
+                $taxes = new Taxes();
+                $taxes->update($user->id_user,$updateTax);
+                new RestResponse($taxes->get($user->id_user),200,'updated');
+                break;
+            case 'POST':
+            case 'DELETE':
+                new RestResponse(null, 405, "Method Not Allowed");
+                break;
+        }
+    }
 
 }
--- src/models/Taxes/Taxes.php
+++ src/models/Taxes/Taxes.php
@@ -4,7 +4,22 @@
 namespace App\models\Taxes;
 
 
-class Taxes
+use System\Model;
+
+class Taxes extends Model
 {
 
+    public function get($idUser){
+        return $this->db()
+            ->table('taxes')
+            ->where('id_user','=',$idUser)
+            ->get();
+    }
+
+    public function update($idUser,$data){
+        $this->db()
+            ->table('taxes')
+            ->where('id_user','=',$idUser)
+            ->update($data);
+    }
 }
