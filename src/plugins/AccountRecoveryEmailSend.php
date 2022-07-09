10a6d60928c2e042e9c2cd079512e4ca122e54bd
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-09 13:53:17 -0400

    refactor
    * Use env var in SMTP configuration

--- src/plugins/AccountRecoveryEmailSend.php
+++ src/plugins/AccountRecoveryEmailSend.php
@@ -4,41 +4,49 @@
 namespace App\plugins;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class AccountRecoveryEmailSend
 {
     private $emailDestinatary;
     private $token;
 
-    public function __construct($emailDestinatary,$token)
+    public function __construct($emailDestinatary, $token)
     {
         $this->emailDestinatary = $emailDestinatary;
         $this->token = $token;
     }
 
-    public function send(){
+    public function send()
+    {
         $mail = new PHPMailer();
-        $mail->IsSMTP(); // enable SMTP
+        try {
+            $mail->IsSMTP(); // enable SMTP
 
-        $mail->SMTPDebug = 0; //debug off = 0, debugging: 1 = errors and messages, 2 = messages only
-        $mail->SMTPAuth = true; // authentication enabled
-        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-        $mail->Host = "mail.gibucket.a2hosted.com";
-        $mail->Port = 465; // or 587
-        $mail->IsHTML(true);
-        $mail->Username = "security@gibucket.a2hosted.com";
-        $mail->Password = "!qLIT=qlc4c_";
-        $mail->SetFrom("security@gibucket.a2hosted.com");
-        $mail->Subject = "Sheiley Shop account recovery";
-        $mail->Body = $this->getBodyMessage();
-        $mail->AddAddress($this->emailDestinatary);
-        return $mail->Send();
+            $mail->SMTPDebug = 0; //debug off = 0, debugging: 1 = errors and messages, 2 = messages only
+            $mail->SMTPAuth = true; // authentication enabled
+
+            $mail->Host = $_ENV['SMTP_HOST'];
+            $mail->Port = $_ENV["SMTP_PORT"]; // or 587
+            $mail->Username = $_ENV['SMTP_EMAIL'];
+            $mail->Password = $_ENV['SMTP_PASSWORD'];
+            $mail->SetFrom($_ENV['SMTP_EMAIL']);
+
+            $mail->IsHTML(true);
+            $mail->Subject = "Sheiley Shop account recovery";
+            $mail->Body = $this->getBodyMessage();
+            $mail->AddAddress($this->emailDestinatary);
+            return $mail->Send();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 
-    private function getBodyMessage(){
+    private function getBodyMessage()
+    {
         $bodyMessage = '<div>The following link will take you to an external page to recover your password;</div>';
-        $url = "https://gibucket.a2hosted.com/sheiley_shop/auth/recoverypassword/?token=$this->token";
-        $bodyMessage .= '<div><a href=" '. $url .' "> ' . $url . '</a></div>';
+        $url = $_ENV["API_FRONTEND_URL"] . "/auth/recoverypassword/?token=$this->token";
+        $bodyMessage .= '<div><a href=" ' . $url . ' "> ' . $url . '</a></div>';
         $bodyMessage .= '<div>This email expires in 2 hours.</div>';
         return $bodyMessage;
     }
